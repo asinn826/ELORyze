fb8f7e7576652964dedf7d8adc7dc74637b2a0ec
Alfred <alfredsin826@hotmail.com>
2014-06-16 22:10:58 -0700

    some more basic changes

---
+++ Backend/Summoner.py
@@ -0,0 +1,8 @@
+class Summoner(object):
+	"""docstring for Summoner"""
+	def __init__(id, name, profileIconId, summonerLevel):
+		self.id = id
+		self.name = name
+		self.profileIconId = profileIconId
+		self.summonerLevel = summonerLevel		
+		
