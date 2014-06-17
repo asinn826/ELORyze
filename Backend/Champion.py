060cd22b3364969530237c577bb59049339b495f
Alfred <alfredsin826@hotmail.com>
2014-06-16 21:33:55 -0700

    Started backend
    Just a placeholder of sorts for now

---
+++ Backend/Champion.py
@@ -0,0 +1,10 @@
+class Champion(object):
+	"""docstring for Champion"""
+	def __init__(self, name):
+		self.name = name
+
+	def setName(name):
+		self.name = name
+
+	def getName():
+		getattr(self, 'name')
