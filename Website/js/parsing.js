a4ac4688c1e4d6c9cf38e20a9eea260abbee1636
Alfred <alfredsin826@hotmail.com>
2014-06-28 01:35:41 -0700

    now we can search and see api info for people

--- Website/js/parsing.js
+++ Website/js/parsing.js
@@ -1,6 +1,8 @@
-var summonerName = "rxneggroll";
-var apiKey = "5ea162cb-a47a-4515-b12d-bcd27e1ac3ae";
-var region = "na"
+var summonerName;
+var apiKey = "5ea162cb-a47a-4515-b12d-bcd27e1ac3ae"; // alfred's api key
+var apiKey2 = "a3fe81d6-1dbc-4b3b-9155-0064a9a76fbe"; // joon's api key
+var apiKey3 = "671a7a2c-2d12-4d1c-a6d1-d13a87be1cb3"; // alfred's other api key
+var region = "na";
 var summonerId;
 	/*
 	Sample response body
@@ -14,33 +16,56 @@ var summonerId;
 	}}
 	Link: https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/rxneggroll?api_key=5ea162cb-a47a-4515-b12d-bcd27e1ac3ae
 	*/
-	
 /*
 *==================================================================
 * THIS RETURNS SUMMONER ID FROM SUMMONER NAME
 *==================================================================
-*/	
+*/
+/*	
 function returnSummonerID(summonerName) {
-	var APIlink = "https://na.api.pvp.net/api/lol/" + region + "/v1.4/summoner/by-name/" + summonerName + "?api_key=" + apiKey;
-	var myRequest = new XMLHttpRequest();
-	myRequest.open("GET", APIlink, false);
-	myRequest.send();
-	var responseObj = JSON.parse(myRequest.responseText);
-	return responseObj[summonerName].id;	
-}
-	
+var APIlink = "https://na.api.pvp.net/api/lol/" + region + "/v1.4/summoner/by-name/" + summonerName + "?api_key=" + apiKey;
+var myRequest = new XMLHttpRequest();
+myRequest.open("GET", APIlink, false);
+myRequest.send();
+var responseObj = JSON.parse(myRequest.responseText);
+return responseObj[summonerName].id;	
+}*/
+
 /*
 *==================================================================
 * THIS RETURNS SUMMONER NAME FROM SUMMONER ID
 *==================================================================
 */
+/*
 function returnSummonerName(summonerID) {
-	var APIlink = "https://na.api.pvp.net/api/lol/" + region + "/v1.4/summoner/" + summonerID + "?api_key=" + apiKey;
-	var myRequest = new XMLHttpRequest();
-	myRequest.open("GET", APIlink, false);
-	myRequest.send();
-	var responseObj = JSON.parse(myRequest.responseText);
-	return responseObj.[summonerID].name;
+var APIlink = "https://na.api.pvp.net/api/lol/" + region + "/v1.4/summoner/" + summonerID + "?api_key=" + apiKey;
+var myRequest = new XMLHttpRequest();
+myRequest.open("GET", APIlink, false);
+myRequest.send();
+var responseObj = JSON.parse(myRequest.responseText);
+return responseObj.[summonerID].name;
+}*/
+
+/*
+*==================================================================
+* Run this first to assign the query to a var
+* jacked the get QueryVariable function from http://css-tricks.com/snippets/javascript/get-url-variables/
+*==================================================================
+*/
+function getQueryVariable(variable)
+{
+   var query = window.location.search.substring(1);
+   var vars = query.split("?");
+   for (var i=0; i<vars.length; i++) {
+           var pair = vars[i].split("=");
+           if(pair[0] == variable){
+           		return pair[1];
+           }
+   }
+   return(false);
+}
+function processSearch() {
+	summonerName = getQueryVariable("query");
 }
 	
 /*
@@ -49,26 +74,34 @@ function returnSummonerName(summonerID) {
 *==================================================================
 */
 function getSummonerName() {
-	var APIlink = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/" + summonerName + "?api_key=" + apiKey;
+	var APIlink = "https://" + region + ".api.pvp.net/api/lol/" + region + "/v1.4/summoner/by-name/" + summonerName + "?api_key=" + apiKey;
 	var myRequest = new XMLHttpRequest();
 	myRequest.open("GET", APIlink, false);
 	myRequest.send();
 	var responseObj = JSON.parse(myRequest.responseText);
 	document.getElementById("summonerName").innerHTML = "<h3>Summoner Name: </h3>" + "<p>" + responseObj[summonerName].name + "</p>";
-} // HOW TO ABSTRACT THE "responseObj.rxneggroll.name", esp the rxneggroll parameter
+}
+
+function setSummonerIdVar() {
+	var APIlink = "https://na.api.pvp.net/api/lol/" + region + "/v1.4/summoner/by-name/" + summonerName + "?api_key=" + apiKey;
+	var myRequest = new XMLHttpRequest();
+	myRequest.open("GET", APIlink, false);
+	myRequest.send();
+	var responseObj = JSON.parse(myRequest.responseText);
+	summonerId = responseObj[summonerName].id;
+}
 
 function getSummonerId() {
-	var APIlink = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/" + summonerName + "?api_key=" + apiKey;
+	var APIlink = "https://na.api.pvp.net/api/lol/" + region + "/v1.4/summoner/by-name/" + summonerName + "?api_key=" + apiKey;
 	var myRequest = new XMLHttpRequest();
 	myRequest.open("GET", APIlink, false);
 	myRequest.send();
 	var responseObj = JSON.parse(myRequest.responseText);
 	document.getElementById("summonerId").innerHTML = "<h3>Summoner Id: </h3>" + "<p>" + responseObj[summonerName].id + "</p>";
-	summonerId = responseObj.rxneggroll.id;
 }
 
 function getSummonerIconNum() {
-	var APIlink = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/" + summonerName + "?api_key=" + apiKey;
+	var APIlink = "https://na.api.pvp.net/api/lol/" + region + "/v1.4/summoner/by-name/" + summonerName + "?api_key=" + apiKey;
 	var myRequest = new XMLHttpRequest();
 	myRequest.open("GET", APIlink, false);
 	myRequest.send();
@@ -110,16 +143,17 @@ function getSummonerNormalsStats() {
 	myRequest.open("GET", APIlink, false);
 	myRequest.send();
 	var responseObj = JSON.parse(myRequest.responseText);
-	document.getElementById("normsWins").innerHTML = "<h3>Ranked Wins: </h3>" + "<p>" + responseObj.playerStatSummaries[7].wins + "</p>";
-	document.getElementById("normsKills").innerHTML = "<h3>Ranked Losses: </h3>" + "<p>" + responseObj.playerStatSummaries[7].aggregatedStats.totalChampionKills + "</p>";
-	document.getElementById("normsCS").innerHTML = "<h3>Ranked Kills: </h3>" + "<p>" + responseObj.playerStatSummaries[7].aggregatedStats.totalMinionKills + "</p>";
-	document.getElementById("normsAssists").innerHTML = "<h3>Ranked Assists: </h3>" + "<p>" + responseObj.playerStatSummaries[7].aggregatedStats.totalAssists + "</p>";
+	document.getElementById("normsWins").innerHTML = "<h3>Normal Wins: </h3>" + "<p>" + responseObj.playerStatSummaries[7].wins + "</p>";
+	document.getElementById("normsKills").innerHTML = "<h3>Normal Losses: </h3>" + "<p>" + responseObj.playerStatSummaries[7].aggregatedStats.totalChampionKills + "</p>";
+	document.getElementById("normsCS").innerHTML = "<h3>Normal Kills: </h3>" + "<p>" + responseObj.playerStatSummaries[7].aggregatedStats.totalMinionKills + "</p>";
+	document.getElementById("normsAssists").innerHTML = "<h3>Normal Assists: </h3>" + "<p>" + responseObj.playerStatSummaries[7].aggregatedStats.totalAssists + "</p>";
 }
 
 
 
-
+processSearch();
 getSummonerName();
+setSummonerIdVar();
 getSummonerId();
 getSummonerIconNum();
 getSummonerLvl();
