2c6e4095252bc8347cf3b62589ab92f2cc7e9193
Alfred <alfredsin826@hotmail.com>
2014-07-04 23:44:43 -0700

    working javascript for live game data

--- Website/js/mashapeexample.js
+++ Website/js/mashapeexample.js
@@ -1,9 +1,5 @@
-<!DOCTYPE html>
-<html>
-<body>
-<script language="javascript" type="text/javascript">
-<!--
-var summonerName = "evokie";
+// CHANGE THIS SHIT
+var summonerName = "rxneggroll";
 
 function testFunc() {
   var APIlink = "https://community-league-of-legends.p.mashape.com/api/v1.0/NA/summoner/retrieveInProgressSpectatorGameInfo/" + summonerName;
@@ -13,11 +9,8 @@ function testFunc() {
 	myRequest.send();
 	var responseObj = JSON.parse(myRequest.responseText);
 	document.write(responseObj.game.playerChampionSelections.array[0].summonerInternalName);
+	document.getElementById("nameOfPlayer").innerHTML = "<h3>Summoner Name: </h3>" + "<p>" + responseObj.game.playerChampionSelections.array[0].summonerInternalName + "</p>";
 
 }	
 
 testFunc();
-//-->
-</script>
-</body>
-</html> 
